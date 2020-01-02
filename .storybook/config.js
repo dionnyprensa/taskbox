732d58b5d2cb335001c44b8c7f544cfc217b0417
Dionny Prensa <d.prensa1@gmail.com>
2020-01-01 21:47:14 -0400

    add red color to task title

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,10 +1,10 @@
 import { configure } from "@storybook/react";
-import requireContext from 'require-context.macro';
+import requireContext from "require-context.macro";
+import "storybook-chromatic";
+
 import "../src/index.css";
 
-// automatically import all files ending in *.stories.js
-// const req = require.context("../src", true, /\.stories.js$/);
-const req = requireContext("../src", true, /\.stories.js$/);
+const req = requireContext("../src/components", true, /\.stories\.js$/);
 
 function loadStories() {
   req.keys().forEach(filename => req(filename));
